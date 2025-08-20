5e45acc63cdbd49f139b73ea9468da7a0ebdb0e1
yashraj <yashraj.kamble@proton.me>
2025-08-20 10:11:04 +0000

    V_2

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo "Updating system..."
 sudo pacman -Syu --noconfirm --quiet --noprogressbar &> /dev/null
 echo "System update completed"
 echo "Inatalling packaging..."
-sudo pacman -S sddm hyprland waybar nwg-dock-hyprland nwg-look brightnessctl ghostty rofi  nemo nemo-fileroller  hyprpaper bluez bluez-utils  blueman sof-firmware lm_sensors hyprpolkitagent  nwg-displays swaync pavucontrol nvidia-dkms nvidia-utils nvidia-prime mesa vulkan-intel vlc git  ninja meson
+sudo pacman -S linux-firmware xf86-video-intel vulkan-intel mesa nvidia-dkms nvidia-utils nvidia-prime mesa vulkan-intel sddm hyprland cinnamon waybar nwg-dock-hyprland nwg-look brightnessctl ghostty rofi  nemo nemo-fileroller  hyprpaper bluez bluez-utils  blueman sof-firmware lm_sensors hyprpolkitagent  nwg-displays swaync pavucontrol vlc git  ninja meson fastfetch
 echo "Packege installation completed"
 
 echo "Installing paru..."
@@ -35,6 +35,7 @@ sudo cp -r -f ~/simplicui/usr/share/* /usr/share/
 echo "Copy config file completed"
 
 sudo usermod -aG input $USER
+sudo systemctl enable sddm
 
 
 
